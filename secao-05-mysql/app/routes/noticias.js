9cfd0b45a833c6762db7e389874b8c7b833f1edf
VitorSchiavo <vitor.schiavo@dasa.com.br>
2019-07-21 15:26:09 -0300

    EJS - Criando views dinâmicas com JavaScript

--- secao-05-mysql/app/routes/noticias.js
+++ secao-05-mysql/app/routes/noticias.js
@@ -1,20 +1,18 @@
 module.exports = function(app) {
 
-app.get('/noticias', function(req, res){
+    app.get('/noticias', function(req, res){
 
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : 'mestre',
-        database : 'portal_noticias'
-    });
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'mestre',
+            database : 'portal_noticias'
+        });
 
-    connection.query('select * from noticias', function(error, result){
-        res.send(result);
-    });
-
-    // res.render('noticias/noticias');
+            connection.query('select * from noticias', function(error, result){
+                res.render("noticias/noticias", {noticias : result});
+            });
     });
 };
 
