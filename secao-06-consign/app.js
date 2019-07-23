4a284e6070d9b4dd369d598741b4517391340226
VitorSchiavo <vitor.schiavo@dasa.com.br>
2019-07-22 22:26:43 -0300

    Refactoring do projeto prático portal de notícias com Consign (routes)

--- secao-06-consign/app.js
+++ secao-06-consign/app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server');
 
-require('./app/routes/noticias')(app);
+// require('./app/routes/noticias')(app);
 
-require('./app/routes/home')(app);
+// require('./app/routes/home')(app);
 
-require('./app/routes/formulario_inclusao_noticias')(app);
+// require('./app/routes/formulario_inclusao_noticias')(app);
 
 app.listen(3000, function(){
     console.log('Servidor Online');
