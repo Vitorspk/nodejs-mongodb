e83ae5c7740f55cd51dff2915ac27959fa956d62
VitorSchiavo <vitor.schiavo@dasa.com.br>
2019-07-26 18:06:54 -0300

    Refactoring do projeto prático portal de notícias com Consign (routes)

--- secao-06-consign/app/routes/noticias.js
+++ secao-06-consign/app/routes/noticias.js
@@ -1,13 +1,13 @@
 var dbConnection = require('../../config/dbConnection');
 
-module.exports = function(app) {
+module.exports = function(app){
 
-    var connection = dbConnection();
+    app.get('/noticias', function(req,res){
 
-    app.get('/noticias', function(req, res){
-
-        connection.query('select * from noticias', function(error, result){
-            res.render("noticias/noticias", {noticias : result});
+        var connection = dbConnection();
+        connection.query("SELECT * FROM noticias", function(error, result){
+            
+            res.render('noticias/noticias', { noticias : result });
         });
     });
 };
