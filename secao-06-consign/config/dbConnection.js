28d92424dbd12d53cd082557b2da47afd46ce5f5
VitorSchiavo <vitor.schiavo@dasa.com.br>
2019-07-26 22:25:49 -0300

    Criando a página de detalhes das notícias

--- secao-06-consign/config/dbConnection.js
+++ secao-06-consign/config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function() {
+var connMyql = function(){
+    console.log('Conexao com o database foi realizada !');
     return mysql.createConnection({
         host : 'localhost',
         user : 'root',
         password : 'mestre',
         database : 'portal_noticias'
     });
-};
+}
+
+module.exports = function() {
+    console.log('O autoload carregou o modulo de conexao com o database');
+    return connMyql;
+}
